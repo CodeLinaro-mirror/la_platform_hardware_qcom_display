976c4871fe82071f1008d2880815bab47f5330a1
Mathew Joseph Karimpanal <mathewjk@codeaurora.org>
2020-04-30 11:55:37 +0530

    composer: Update hotplug uevent message
    Update hotplug uevent message reading to align with upstream's unified
    scheme for SST and MST.
    
    Change-Id: I1866cd4421e66e854ac02d4ee6aa4e5661774a0d

--- sdm/libs/hwc2/hwc_session.cpp
+++ sdm/libs/hwc2/hwc_session.cpp
@@ -2474,6 +2474,8 @@ void HWCSession::UEventHandler(const char *uevent_data, int length) {
     // MST hotplug will not carry connection status/test pattern etc.
     // Pluggable display handler will check all connection status' and take action accordingly.
     const char *str_status = GetTokenValue(uevent_data, length, "status=");
+    const char *str_sstmst = GetTokenValue(uevent_data, length, "HOTPLUG=");
+    // Check MST_HOTPLUG for backward compatibility.
     const char *str_mst = GetTokenValue(uevent_data, length, "MST_HOTPLUG=");
     const char *str_plane = GetTokenValue(uevent_data, length, "PLANE_POSSIBLE_CRTCS_UPDATED");
     const char *str_handoff = GetTokenValue(uevent_data, length, "PLANE_HANDOFF_REQUESTED=");
@@ -2495,14 +2497,16 @@ void HWCSession::UEventHandler(const char *uevent_data, int length) {
       return;
     }
 
-    if (!str_status && !str_mst) {
+    if (!str_status && !str_mst && !str_sstmst) {
       return;
     }
 
     hpd_bpp_ = GetEventValue(uevent_data, length, "bpp=");
     hpd_pattern_ = GetEventValue(uevent_data, length, "pattern=");
-    DLOGI("Uevent = %s, status = %s, MST_HOTPLUG = %s, bpp = %d, pattern = %d", uevent_data,
-          str_status ? str_status : "NULL", str_mst ? str_mst : "NULL", hpd_bpp_, hpd_pattern_);
+    DLOGI("UEvent = %s, status = %s, HOTPLUG = %s (SST/MST)%s%s, bpp = %d, pattern = %d",
+          uevent_data, str_status ? str_status : "NULL", str_sstmst ? str_sstmst : "NULL",
+          str_mst ? ", MST_HOTPLUG = " : "", str_mst ? str_mst : "",
+          hpd_bpp_, hpd_pattern_);
 
     int virtual_display_index =
         GetDisplayIndex(qdutils::DISPLAY_VIRTUAL);
