086067eb35c0474bac084429e582821c96457885
Xiaowen Wu <wxiaowen@codeaurora.org>
2020-07-14 11:40:26 -0400

    hwc2: refresh when geometry change happens during sideband update
    Refresh when geometry change happens during sideband update. This
    will trigger another commit from SurfaceFlinger to fix the state.
    
    Change-Id: I83728075c14f4977ed067a5328f7480a83fbb94b

--- sdm/libs/hwc2/hwc_sideband.cpp
+++ sdm/libs/hwc2/hwc_sideband.cpp
@@ -530,8 +530,10 @@ int32_t HWCSidebandStreamSession::UpdateSidebandStreamLocked(HWCSidebandStream *
     if (!(pendingMask & (1 << display)))
       continue;
 
-    if (hwc_session->hwc_display_[display]->GetGeometryChanges())
+    if (hwc_session->hwc_display_[display]->GetGeometryChanges()) {
+      refresh_list->push_back(display);
       continue;
+    }
 
     int32_t retire_fence;
     status = hwc_session->hwc_display_[display]->SidebandStreamPresent(&retire_fence);
