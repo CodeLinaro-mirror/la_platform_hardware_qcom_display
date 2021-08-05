dfee8e541b01c7ac5dd60f3e049b5ea262cb5d9c
Asmeer Mohammed <asmemoha@codeaurora.org>
2021-08-05 20:05:27 +0530

    Memtrack 1.0 HIDL deprecated
    For Android S FCM target-level 6, memtrack hidl is deprecated
    replaced with AIDL Hal
    
    Change-Id: Ie4481ce417b0e8cad1b867b25c279e1454148c34

--- config/display-product.mk
+++ config/display-product.mk
@@ -5,8 +5,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack-service.example \
     gralloc.$(TARGET_BOARD_PLATFORM) \
     lights.$(TARGET_BOARD_PLATFORM) \
     hwcomposer.$(TARGET_BOARD_PLATFORM) \
