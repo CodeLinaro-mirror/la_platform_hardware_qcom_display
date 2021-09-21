3d0b83db09d90c89234425ceafce6262ef98db13
Baldev Sahu <bsahu@codeaurora.org>
2021-09-21 15:43:28 +0530

    hwc2: Add support for getActiveBuiltinDisplayAttributes
    This support is required as secureUI calls this api to query
    display attributes.
    
    Change-Id: Ie7d9ba71588fcab3fb73b6b51ec8e99efda20eea

--- sdm/libs/hwc2/hwc_session_services.cpp
+++ sdm/libs/hwc2/hwc_session_services.cpp
@@ -27,6 +27,42 @@
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+* Changes from Qualcomm Innovation Center are provided under the following license:
+*
+* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted (subject to the limitations in the
+* disclaimer below) provided that the following conditions are met:
+*
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*
+*    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+*
+* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <core/buffer_allocator.h>
 #include <utils/debug.h>
 #include <sync/sync.h>
@@ -239,6 +275,34 @@ int HWCSession::SetActiveConfigIndex(int disp_id, uint32_t config) {
 
 int HWCSession::DisplayConfigImpl::SetActiveConfig(DispType dpy, uint32_t config) {
   return hwc_session_->SetActiveConfigIndex(MapDisplayType(dpy), config);
+}
+
+int HWCSession::DisplayConfigImpl::GetActiveBuiltinDisplayAttributes(
+                                           DisplayConfig::Attributes *attributes) {
+  int32_t error = -EINVAL;
+  hwc2_display_t disp_id = HWC_DISPLAY_PRIMARY;
+
+  if (hwc_session_->hwc_display_[disp_id]) {
+    uint32_t config_index = 0;
+    HWC2::Error ret = hwc_session_->hwc_display_[disp_id]->GetActiveConfig(&config_index);
+    if (ret != HWC2::Error::None) {
+      goto err;
+    }
+    DisplayConfigVariableInfo hwc_display_attributes;
+    error = hwc_session_->hwc_display_[disp_id]->GetDisplayAttributesForConfig(
+                          static_cast<int>(config_index), &hwc_display_attributes);
+    if (!error) {
+      attributes->vsync_period = hwc_display_attributes.vsync_period_ns;
+      attributes->x_res = hwc_display_attributes.x_pixels;
+      attributes->y_res = hwc_display_attributes.y_pixels;
+      attributes->x_dpi = hwc_display_attributes.x_dpi;
+      attributes->y_dpi = hwc_display_attributes.y_dpi;
+      attributes->panel_type = DisplayConfig::DisplayPortType::kDefault;
+      attributes->is_yuv = hwc_display_attributes.is_yuv;
+    }
+  }
+err:
+  return error;
 }
 
 int HWCSession::DisplayConfigImpl::GetDisplayAttributes(uint32_t config_index, DispType dpy,
