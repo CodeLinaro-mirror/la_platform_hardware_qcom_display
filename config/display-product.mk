c179ae76620234c363ae66f8f24d6e1b3dbe27fc
Gurkaran Singh <gurksing@codeaurora.org>
2021-10-11 12:31:11 +0530

    vendor-freeze: Move few commonsys-intf libs to commonsys
    Moved libdisplayconfig.qti, libqdMetadata, and libgralloc.qti from
    vendor/qcom/opensource/commonsys-intf/display to
    hardware/qcom/display to install in vendor
    Change-Id: I0627decb40a2d355157ac6909479765781cbdd6b
    Signed-off-by: Rahul Sharma <rahsha@codeaurora.org>
    LNXDISPLAY-1552: REQ002
    CRs-Fixed: 2972776

--- config/display-product.mk
+++ config/display-product.mk
@@ -12,6 +12,11 @@ PRODUCT_PACKAGES += \
     libqdMetaData.vendor \
     libdisplayconfig.vendor \
     libdisplayconfig.qti.vendor \
+    libqdMetaData \
+    libqdMetaData.system \
+    libdisplayconfig \
+    libgralloc.qti \
+    libdisplayconfig.qti \
     vendor.display.config@1.0.vendor \
     vendor.display.config@1.1.vendor \
     vendor.display.config@1.2.vendor \
