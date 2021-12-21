9c97765b9fe2a5dc6ce62df9ffd3b8c3b5a5bf35
Swastik Ingle <single@qti.qualcomm.com>
2021-12-21 19:20:25 +0530

    composer: add null display resolution selection algorithm
    If the device boots up with non-primary pluggable displays
    and no primary display, the algorithms iterates through
    available connected displays for best resolution to set
    for primary's dummy display.
    
    Change-Id: I55f15f7faa503261c3a706bb7ed7a5bbea523ec9

--- composer/hwc_session.h
+++ composer/hwc_session.h
@@ -485,6 +485,7 @@ class HWCSession : public HWCUEvent,
   bool HasHDRSupport(HWCDisplay *hwc_display);
   void PostInit();
   int GetDispTypeFromPhysicalId(uint64_t physical_disp_id, DispType *disp_type);
+  int SetBestNullDisplayResolution();
 #ifdef PROFILE_COVERAGE_DATA
   android::status_t DumpCodeCoverage(const android::Parcel *input_parcel);
 #endif
