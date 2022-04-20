06af060c9738ae27eb76b3f3dd6e1c919da45f55
Lan Zou <quic_lanz@quicinc.com>
2022-04-20 16:06:47 +0800

    sdm: add NULL pointer checks
    The code is ported from change 3545336 in display.lnx.8.0.
    This branch need this to fix kw issue.
    
    Change-Id: Ie722ef863ed653d90f86b4c4297087ed3b5bcc16
    Signed-off-by: Lan Zou <quic_lanz@quicinc.com>

--- sdm/libs/core/drm/hw_info_drm.cpp
+++ sdm/libs/core/drm/hw_info_drm.cpp
@@ -128,7 +128,14 @@ DisplayError HWInfoDRM::Init() {
       return kErrorCriticalResource;
     }
     drm_master->GetHandle(&dev_fd);
-    DRMLibLoader::GetInstance()->FuncGetDRMManager()(dev_fd, &drm_mgr_intf_);
+
+    DRMLibLoader *drm_lib_loader = DRMLibLoader::GetInstance();
+    if (!drm_lib_loader) {
+      DLOGE("Failed to acquire DRMLibLoader instance");
+      DRMMaster::DestroyInstance(card_id_);
+      return kErrorCriticalResource;
+    }
+    drm_lib_loader->FuncGetDRMManager()(dev_fd, &drm_mgr_intf_);
     if (!drm_mgr_intf_) {
       DRMLibLoader::Destroy();
       DRMMaster::DestroyInstance(card_id_);
@@ -150,7 +157,9 @@ void HWInfoDRM::Deinit() {
   }
 
   if (drm_mgr_intf_) {
-    DRMLibLoader::GetInstance()->FuncDestroyDRMManager()();
+    DRMLibLoader *drm_lib_loader = DRMLibLoader::GetInstance();
+    if (drm_lib_loader)
+      drm_lib_loader->FuncDestroyDRMManager()();
     drm_mgr_intf_ = nullptr;
   }
 
