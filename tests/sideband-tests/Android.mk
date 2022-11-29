aa7072b455a2559b5abc4669d4822a1fdea7f01f
Chirag Khurana <quic_ckhurana@quicinc.com>
2022-11-30 00:13:33 +0530

    tests: sideband: implement SF Client based test app
    Change-Id: I19c07cb78ec3ca222c910c9a8617458e00bd388d

--- tests/sideband-tests/Android.mk
+++ tests/sideband-tests/Android.mk
@@ -4,19 +4,19 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE           := sideband-test
 LOCAL_MODULE_TAGS      := tests
-LOCAL_CFLAGS           := -DLOG_TAG=\"SidebandTest\"  -fstack-protector-all \
-                          -g  -Wall -Wextra -Werror -fno-builtin -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS           := -DLOG_TAG=\"SidebandTest\" -fstack-protector-all \
+                          -g -Wall -Wextra -Werror -fno-builtin -Wno-unused-parameter -Wno-unused-variable
 
-LOCAL_SHARED_LIBRARIES := libcutils libutils liblog libhidlbase libsync \
-                          libgrallocutils \
+LOCAL_SHARED_LIBRARIES := libcutils libutils liblog libhidlbase libsync libgui libui libbinder \
                           android.hardware.graphics.allocator@2.0 \
                           android.hardware.graphics.mapper@2.0 \
                           android.hardware.graphics.common@1.1 \
                           vendor.display.config@1.21
 
+LOCAL_STATIC_LIBRARIES := libgtest
+
 LOCAL_HEADER_LIBRARIES := display_headers
-LOCAL_SRC_FILES        := sideband_test.cpp
-LOCAL_VENDOR_MODULE    := true
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_NATIVE_TESTS)/display-tests
+LOCAL_SRC_FILES        := sideband_test_helper.cpp sideband_test.cpp
+LOCAL_MODULE_PATH      := $(TARGET_OUT_DATA_NATIVE_TESTS)/display-tests
 
 include $(BUILD_EXECUTABLE)
--- tests/sideband-tests/sideband_test.cpp
+++ tests/sideband-tests/sideband_test.cpp
@@ -32,310 +32,111 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <map>
-#include <sys/mman.h>
-#include <gr_utils.h>
-#include <iostream>
-#include <sync/sync.h>
+#include <gtest/gtest.h>
 
-#include <android/hardware/graphics/allocator/2.0/IAllocator.h>
-#include <android/hardware/graphics/mapper/2.0/IMapper.h>
+#include <android/native_window.h>
+#include <gui/Surface.h>
+#include <gui/SurfaceComposerClient.h>
 
-#include <vendor/display/config/1.21/IDisplayConfig.h>
+#include <ui/DisplayInfo.h>
 
-using namespace android;
-using android::sp;
-using vendor::display::config::V1_21::IDisplayConfig;
+#include "sideband_test.h"
 
-using android::hardware::hidl_handle;
-using android::hardware::hidl_vec;
-using android::hardware::graphics::allocator::V2_0::IAllocator;
-using android::hardware::graphics::common::V1_0::PixelFormat;
-using android::hardware::graphics::common::V1_1::BufferUsage;
-using android::hardware::graphics::mapper::V2_0::BufferDescriptor;
-using android::hardware::graphics::mapper::V2_0::IMapper;
-using android::hardware::graphics::mapper::V2_0::Error;
-using android::hardware::graphics::mapper::V2_0::YCbCrLayout;
-using std::vector;
+namespace android {
 
-static const int kNumMsec = 16660;
-static const int kNumBuffers = 30;
-volatile sig_atomic_t stop;
-// Wraps shared gralloc functions
-class TunnellingHelper {
- public:
-  TunnellingHelper();
-  ~TunnellingHelper() = default;
+using Transaction = SurfaceComposerClient::Transaction;
+using ui::ColorMode;
 
-  vector<const native_handle_t *> Allocate(const IMapper::BufferDescriptorInfo &desc_info,
-                                           uint32_t count, uint32_t *out_stride = nullptr);
+namespace {
+const String8 SURFACE_NAME("Sideband Test Surface");
+} // namespace
 
-  const native_handle_t *ImportBuffer(const hidl_handle &raw_handle);
-  void FreeBuffer(const native_handle_t *buffer_handle);
+/**
+ * This class tests the SetLayerSidebandStream method in SurfaceFlinger.
+ */
+class SidebandStreamTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        ASSERT_NO_FATAL_FAILURE(initClient());
+    }
 
-  BufferDescriptor CreateDescriptor(const IMapper::BufferDescriptorInfo &descriptorInfo);
+    void TearDown() override {
+        mComposerClient->dispose();
+        mSurfaceControl.clear();
+        mComposerClient.clear();
+    }
 
-  std::map<private_handle_t *,int > handle_release_fence_map_;
+    sp<IBinder> mDisplay;
+    sp<SurfaceComposerClient> mComposerClient = nullptr;
+    sp<SurfaceControl> mSurfaceControl = nullptr;
+    SurfaceComposerClient::Transaction transaction = {};
 
-private:
-  void Init();
-  sp<IAllocator> allocator_;
-  sp<IMapper> mapper_;
+    void initClient() {
+        mComposerClient = new SurfaceComposerClient;
+        ASSERT_EQ(NO_ERROR, mComposerClient->initCheck());
+    }
+
+    void setupSidebandSurface(int width=500, int height=500,
+                              int z=INT_MAX-3, bool fullscreen=false) {
+        mDisplay = SurfaceComposerClient::getInternalDisplayToken();
+        ASSERT_FALSE(mDisplay == nullptr);
+
+        DisplayInfo info;
+        ASSERT_EQ(NO_ERROR, SurfaceComposerClient::getDisplayInfo(mDisplay, &info));
+        const ssize_t surfaceWidth = fullscreen ? info.w : width;
+        const ssize_t surfaceHeight = fullscreen ? info.h : height;
+
+        mSurfaceControl =
+                mComposerClient->createSurface(SURFACE_NAME, surfaceWidth, surfaceHeight,
+                                               PIXEL_FORMAT_RGB_888, 0);
+        ASSERT_TRUE(mSurfaceControl != nullptr);
+        ASSERT_TRUE(mSurfaceControl->isValid());
+
+        transaction = {};
+        ASSERT_EQ(NO_ERROR,
+                  transaction.setLayer(mSurfaceControl, z).apply());
+
+        transaction = {};
+        sp<Surface> surface = mSurfaceControl->getSurface();
+        ANativeWindow_Buffer surfaceBuffer;
+        surface->lock(&surfaceBuffer, NULL);
+        size_t buf_size = surfaceBuffer.stride * surfaceBuffer.height *
+                          bytesPerPixel(surfaceBuffer.format);
+        printf("width: %d, height: %d, stride: %d, format: %d\n", surfaceBuffer.width,
+                surfaceBuffer.height, surfaceBuffer.stride, surfaceBuffer.format);
+        ASSERT_EQ(NO_ERROR, transaction.apply());
+
+        transaction = {};
+        memset(surfaceBuffer.bits, 0x00, buf_size);
+        ASSERT_EQ(NO_ERROR, transaction.apply());
+
+        transaction = {};
+        sp<ANativeWindow> window(surface);
+        ANativeWindowBuffer *anw;
+        native_window_api_connect(window.get(), NATIVE_WINDOW_API_CPU);
+        native_window_dequeue_buffer_and_wait(window.get(), &anw);
+        sp<NativeHandle> stream = android::NativeHandle::create(
+                                const_cast<native_handle*>(anw->handle), false);
+        surface->setSidebandStream(stream);
+        ASSERT_EQ(NO_ERROR, transaction.apply());
+
+        surface->unlockAndPost();
+    }
+
+    void queueBuffers() {
+        send_buffers();
+    }
 };
 
-TunnellingHelper::TunnellingHelper() {
-  Init();
+TEST_F(SidebandStreamTest, QueueSidebandStreamBufferTest) {
+    setupSidebandSurface();
+    printf("Surface Sideband Stream setup successfully, now queueing buffers...\n");
+    queueBuffers();
 }
 
-void TunnellingHelper::Init() {
-  allocator_ = IAllocator::getService();
-  mapper_ = IMapper::castFrom(IMapper::getService());
-  if (allocator_.get() == nullptr) {
-    std::cout << "failed to get allocator service";
-    return;
-  }
-  if (mapper_.get() == nullptr) {
-    std::cout << "failed to get mapper service";
-    return;
-  }
-}
-
-vector<const native_handle_t *> TunnellingHelper::Allocate(
-    const IMapper::BufferDescriptorInfo &desc_info, uint32_t count, uint32_t *out_stride) {
-  vector<const native_handle_t *> handles;
-  auto descriptor = CreateDescriptor(desc_info);
-  allocator_->allocate(
-      descriptor, count,
-      [&](const auto &_error, const auto &_stride, const auto &_buffers) {
-        if (Error::NONE != _error) {
-           std::cout << "failed to allocate buffers";
-           return;
-        }
-        if (count != _buffers.size()) {
-           std::cout << "invalid buffer array";
-           return;
-        }
-        for (uint32_t i = 0; i < count; i++) {
-          handles.push_back(ImportBuffer(_buffers[i]));
-        }
-        if (out_stride) {
-          *out_stride = _stride;
-        }
-      });
-
-  return handles;
-}
-
-const native_handle_t *TunnellingHelper::ImportBuffer(const hidl_handle &raw_handle) {
-  const native_handle_t *handle = nullptr;
-  mapper_->importBuffer(raw_handle, [&](const auto &_error,
-                                        const auto &_buffer) {
-    if (Error::NONE != _error) {
-       std::cout << "failed to import buffer %p" << raw_handle.getNativeHandle();
-       return;
-    }
-    handle = static_cast<const native_handle_t *>(_buffer);
-  });
-
-  return handle;
-}
-
-void TunnellingHelper::FreeBuffer(const native_handle_t *buffer_handle) {
-  auto buffer = const_cast<native_handle_t *>(buffer_handle);
-  Error error =
-      mapper_->freeBuffer(buffer);
-  if (Error::NONE != error) {
-     std::cout << "failed to free buffer " << buffer;
-     return;
-  }
-}
-
-BufferDescriptor TunnellingHelper::CreateDescriptor(
-  const IMapper::BufferDescriptorInfo &descriptor_info) {
-  auto descriptor = BufferDescriptor();
-  mapper_->createDescriptor(descriptor_info, [&](const auto &_error,
-                                                 const auto &_descriptor) {
-    if (Error::NONE != _error) {
-       std::cout << "failed to create descriptor";
-       return;
-    }
-    descriptor = _descriptor;
-  });
-  return descriptor;
-}
-
-void memset24(void *p_dst, uint32_t value, int count)
-{
-  uint8_t *ptr = (uint8_t *)p_dst,*end_ptr;
-  uint8_t x, y, z;
-
-  end_ptr = ptr + 3 * count;
-  x = value & 0xFF;
-  y = (value >> 8) & 0xFF;
-  z = (value >> 16) & 0xFF;
-
-  while (ptr < end_ptr) {
-    *ptr++ = x;
-    *ptr++ = y;
-    *ptr++ = z;
-  }
-}
-
-void sigint_handler(int signum)
-{
-  signal(SIGINT, SIG_IGN);
-  stop = 1;
-  return;
-}
+} // namespace android
 
 int main(int argc, char **argv) {
-  IMapper::BufferDescriptorInfo info = {
-    .width = 500,
-    .height = 500,
-    .layerCount = 1,
-    .format = static_cast<PixelFormat>(HAL_PIXEL_FORMAT_RGB_888),
-    .usage = static_cast<uint64_t>(BufferUsage::CPU_WRITE_MASK),
-  };
-  std::unique_ptr<TunnellingHelper> gralloc_;
-  gralloc_ = std::make_unique<TunnellingHelper>();
-  sp<IDisplayConfig>  mDisplayConfig = IDisplayConfig::getService();
-  if (mDisplayConfig == NULL) {
-    std::cout << "Could not load service IDisplayConfig" <<std::endl;
-    return 0;
-  }
-  auto handles = gralloc_->Allocate(info, kNumBuffers);
-  int fd[kNumBuffers];
-  for(int i = 0; i < kNumBuffers; i++) {
-    auto hnd = (private_handle_t *) handles[i];
-    gralloc_->handle_release_fence_map_[hnd] = -1;
-    hnd->Dump(hnd);
-    fd[i] = hnd->fd;
-    int width = hnd->width;
-    int height = hnd->height;
-    unsigned int size = hnd->size;
-    printf("Buffer fd = %d, width x height: %d x %d, size = %d\n",
-            hnd->fd, width, height, size);
-    void* cpudaddr = mmap(NULL, size, PROT_READ | PROT_WRITE,
-                          MAP_SHARED, fd[i], 0);
-    memset(cpudaddr, 0x00, size);
-
-    char *buffer = ((char *) cpudaddr);
-    uint32_t long_color_val;
-    if (i < kNumBuffers/3)
-      long_color_val = 0xFFFF00;
-    else if (i < (2 * (kNumBuffers/3)))
-      long_color_val = 0xFFFFFF;
-    else
-      long_color_val = 0x000000;
-    for (int row = 0; row < height; row++) {
-      memset24(buffer + (width * 3) * row, long_color_val, width);
-    }
-  }
-
-  IDisplayConfig::LayerInfo layer;
-  layer.z_order = 3000;
-  layer.dataspace = HAL_DATASPACE_SRGB;
-  layer.layer_transform = IDisplayConfig::transform_t::TRANSFORM_FLIP_H;
-  layer.dst_rect.left = 0;
-  layer.dst_rect.top = 0;
-  layer.dst_rect.right = 500;
-  layer.dst_rect.bottom = 500;
-
-  layer.src_rect.left = 0;
-  layer.src_rect.top = 0;
-  layer.src_rect.right = 500;
-  layer.src_rect.bottom = 500;
-
-  layer.buffer_fd = fd[0];
-  layer.width = 512;
-  layer.height = 500;
-  layer.unaligned_width = 500;
-  layer.unaligned_height = 500;
-  layer.format = HAL_PIXEL_FORMAT_RGB_888;
-  layer.plane_alpha = 1;
-  layer.blending = IDisplayConfig::blend_mode::BLEND_MODE_PREMULTIPLIED;
-
-  int err = mDisplayConfig->tunnellingInit();
-  if (err != 0) {
-    return 0;
-  }
-
-  err = mDisplayConfig->createTunnelledLayer(layer);
-  if (err != 0) {
-    return 0;
-  }
-
-  signal(SIGINT, sigint_handler);
-
-  while(!stop) {
-    for(int i = 0; i < kNumBuffers; i++) {
-      auto hnd = (private_handle_t *)handles[i];
-      int release_fence_fd = gralloc_->handle_release_fence_map_[hnd];
-      if (release_fence_fd >= 0) {
-        err = sync_wait(release_fence_fd, 16);
-        if (err < 0) {
-          ALOGE("Fence did not signal, fd : %d",release_fence_fd);
-        } else {
-          ALOGE("Fence signalled, fd : %d",release_fence_fd);
-        }
-        close(release_fence_fd);
-      }
-      ALOGI("Before queue buffer");
-      // Client should pass real acquire fence handle below instead of NULL
-      // NULL acquire fence handle will be considered as -1 acquire fence fd
-      auto error = mDisplayConfig->queueTunnelledBuffer(hnd, NULL /*acquire_fence_handle*/);
-      if (!error.isOk())
-        continue;
-
-      if (error == EINVAL) {
-        stop = true;
-        break;
-      }
-
-      usleep(kNumMsec);
-      int32_t release_fence = -1;
-      ALOGI("Before dequeue buffer\n");
-      auto error_dequeue = mDisplayConfig->dequeueTunnelledBuffer(hnd, [&](const auto& tmpError,
-         const auto& tmpHandle) {
-         err = tmpError;
-         if (tmpHandle != NULL) {
-           const native_handle_t* nativeFenceHandle = tmpHandle.getNativeHandle();
-           if (nativeFenceHandle != nullptr) {
-             release_fence = dup(nativeFenceHandle->data[0]);
-             close(nativeFenceHandle->data[0]);
-             native_handle_close(nativeFenceHandle);
-           }
-         }
-      });
-      if (!error_dequeue.isOk())
-         continue;
-
-      if (err == EINVAL) {
-        stop = true;
-        break;
-      }
-
-      if (release_fence != -1) {
-        gralloc_->handle_release_fence_map_[hnd] = release_fence;
-        ALOGI("Duped fd = %d ...for hnd i = %d", release_fence,i);
-      }
-    }
-  }
-
-  err = mDisplayConfig->destroyTunnelledLayer();
-  if (err != 0) {
-    ALOGE("Destroy layer failed");
-  }
-
-  err = mDisplayConfig->tunnellingDeinit();
-  if (err != 0) {
-    ALOGE("tunnellingDeinit failed");
-  }
-
-  for(int i = 0; i < kNumBuffers; i++) {
-    gralloc_->FreeBuffer(handles[i]);
-  }
-
-  mDisplayConfig.clear();
-  mDisplayConfig = NULL;
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
