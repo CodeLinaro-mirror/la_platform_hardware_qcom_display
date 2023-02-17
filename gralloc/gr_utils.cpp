9f66aac45ea147755282346017364f9eb4d6e0ad
Swastik Ingle <quic_single@quicinc.com>
2023-02-17 05:39:15 -0800

    gralloc: Add check for width and height calculation
    Width and height calculation of a buffer should always be greater
    than zero, so add check for valid width and
    height and return "-1" to caller if width or height
    is not a positive value.
    
    Change-Id: Ib0f0ca41659be839f48ce7f166b0b1aeaec51828
    CRs-Fixed: 2643839

--- gralloc/gr_utils.cpp
+++ gralloc/gr_utils.cpp
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -436,6 +436,13 @@ unsigned int GetSize(const BufferInfo &info, unsigned int alignedw, unsigned int
 int GetBufferSizeAndDimensions(const BufferInfo &info, unsigned int *size, unsigned int *alignedw,
                                unsigned int *alignedh) {
   GraphicsMetadata graphics_metadata = {};
+  if (info.width < 1 || info.height < 1) {
+    *alignedw = 0;
+    *alignedh = 0;
+    *size = 0;
+    ALOGW("%s: Invalid buffer info, Width: %d, Height: %d.", __FUNCTION__, info.width, info.height);
+    return -1;
+  }
   return GetBufferSizeAndDimensions(info, size, alignedw, alignedh, &graphics_metadata);
 }
 
@@ -445,7 +452,11 @@ int GetBufferSizeAndDimensions(const BufferInfo &info, unsigned int *size, unsig
   if (CanUseAdrenoForSize(buffer_type, info.usage)) {
     return GetGpuResourceSizeAndDimensions(info, size, alignedw, alignedh, graphics_metadata);
   } else {
-    GetAlignedWidthAndHeight(info, alignedw, alignedh);
+    int err = GetAlignedWidthAndHeight(info, alignedw, alignedh);
+    if (err) {
+      *size = 0;
+      return err;
+    }
     *size = GetSize(info, *alignedw, *alignedh);
   }
   return 0;
@@ -648,7 +659,10 @@ int GetYUVPlaneInfo(const private_handle_t *hnd, struct android_ycbcr ycbcr[2]) 
   BufferDim_t buffer_dim;
   if (getMetaData(const_cast<private_handle_t *>(hnd), GET_BUFFER_GEOMETRY, &buffer_dim) == 0) {
     BufferInfo info(buffer_dim.sliceWidth, buffer_dim.sliceHeight, format, usage);
-    GetAlignedWidthAndHeight(info, &width, &height);
+    err = GetAlignedWidthAndHeight(info, &width, &height);
+    if (err) {
+      return err;
+    }
   }
 
   // Check metadata for interlaced content.
@@ -980,10 +994,9 @@ int GetRgbDataAddress(private_handle_t *hnd, void **rgb_data) {
   return err;
 }
 
-void GetCustomDimensions(private_handle_t *hnd, int *stride, int *height) {
+int GetCustomDimensions(private_handle_t *hnd, int *stride, int *height) {
   BufferDim_t buffer_dim;
   int interlaced = 0;
-
   *stride = hnd->width;
   *height = hnd->height;
   if (getMetaData(hnd, GET_BUFFER_GEOMETRY, &buffer_dim) == 0) {
@@ -995,11 +1008,17 @@ void GetCustomDimensions(private_handle_t *hnd, int *stride, int *height) {
       // Get re-aligned height for single ubwc interlaced field and
       // multiply by 2 to get frame height.
       BufferInfo info(hnd->width, ((hnd->height + 1) >> 1), hnd->format);
-      GetAlignedWidthAndHeight(info, &alignedw, &alignedh);
+      int err = GetAlignedWidthAndHeight(info, &alignedw, &alignedh);
+      if (err) {
+        *stride = 0;
+        *height = 0;
+        return err;
+      }
       *stride = static_cast<int>(alignedw);
       *height = static_cast<int>(alignedh * 2);
     }
   }
+  return 0;
 }
 
 void GetColorSpaceFromMetadata(private_handle_t *hnd, int *color_space) {
@@ -1025,13 +1044,18 @@ void GetColorSpaceFromMetadata(private_handle_t *hnd, int *color_space) {
   }
 }
 
-void GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
+int GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
                               unsigned int *alignedh) {
   int width = info.width;
   int height = info.height;
   int format = info.format;
   uint64_t usage = info.usage;
-
+  if (width < 1 || height < 1) {
+    *alignedw = 0;
+    *alignedh = 0;
+    ALOGW("%s: Invalid buffer info, Width: %d, Height: %d.", __FUNCTION__, width, height);
+    return -1;
+  }
   // Currently surface padding is only computed for RGB* surfaces.
   bool ubwc_enabled = IsUBwcEnabled(format, usage);
   int tile = ubwc_enabled;
@@ -1051,7 +1075,7 @@ void GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
           __FUNCTION__, width, height, format, result);
       *alignedw = width;
       *alignedh = aligned_h;
-      return;
+      return result;
     }
 
     result = CameraInfo::GetInstance()->GetScanline(format, (PlaneComponent)PLANE_COMPONENT_Y,
@@ -1063,12 +1087,12 @@ void GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
           __FUNCTION__, width, height, format, result);
       *alignedw = aligned_w;
       *alignedh = height;
-      return;
+      return result;
     }
 
     *alignedw = aligned_w;
     *alignedh = aligned_h;
-    return;
+    return 0;
   }
 
   if (IsUncompressedRGBFormat(format)) {
@@ -1076,19 +1100,19 @@ void GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
       AdrenoMemInfo::GetInstance()->AlignUnCompressedRGB(width, height, format, tile, alignedw,
                                                          alignedh);
     }
-    return;
+    return 0;
   }
 
   if (ubwc_enabled) {
     GetYuvUBwcWidthAndHeight(width, height, format, alignedw, alignedh);
-    return;
+    return 0;
   }
 
   if (IsCompressedRGBFormat(format)) {
     if (AdrenoMemInfo::GetInstance()) {
       AdrenoMemInfo::GetInstance()->AlignCompressedRGB(width, height, format, alignedw, alignedh);
     }
-    return;
+    return 0;
   }
 
   int aligned_w = width;
@@ -1100,7 +1124,8 @@ void GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
     case HAL_PIXEL_FORMAT_YCrCb_420_SP:
     case HAL_PIXEL_FORMAT_YCbCr_420_SP:
       if (AdrenoMemInfo::GetInstance() == nullptr) {
-        return;
+        ALOGW("%s: AdrenoMemInfo instance pointing to a NULL value.", __FUNCTION__);
+        return -1;
       }
       alignment = AdrenoMemInfo::GetInstance()->GetGpuPixelAlignment();
       aligned_w = ALIGN(width, alignment);
@@ -1126,6 +1151,16 @@ void GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
       aligned_w = ALIGN(width, 128);
       break;
     case HAL_PIXEL_FORMAT_YV12:
+       if ((usage & BufferUsage::GPU_TEXTURE) || (usage & BufferUsage::GPU_RENDER_TARGET)) {
+        if (AdrenoMemInfo::GetInstance() == nullptr) {
+          return -1;
+        }
+        alignment = AdrenoMemInfo::GetInstance()->GetGpuPixelAlignment();
+        aligned_w = ALIGN(width, alignment);
+      } else {
+        aligned_w = ALIGN(width, 16);
+      }
+      break;
     case HAL_PIXEL_FORMAT_YCbCr_422_SP:
     case HAL_PIXEL_FORMAT_YCrCb_422_SP:
     case HAL_PIXEL_FORMAT_YCbCr_422_I:
@@ -1165,6 +1200,7 @@ void GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
 
   *alignedw = (unsigned int)aligned_w;
   *alignedh = (unsigned int)aligned_h;
+  return 0;
 }
 
 int GetBufferLayout(private_handle_t *hnd, uint32_t stride[4], uint32_t offset[4],
@@ -1239,7 +1275,10 @@ int GetBufferLayout(private_handle_t *hnd, uint32_t stride[4], uint32_t offset[4
 int GetGpuResourceSizeAndDimensions(const BufferInfo &info, unsigned int *size,
                                     unsigned int *alignedw, unsigned int *alignedh,
                                     GraphicsMetadata *graphics_metadata) {
-  GetAlignedWidthAndHeight(info, alignedw, alignedh);
+  int err = GetAlignedWidthAndHeight(info, alignedw, alignedh);
+  if (err) {
+    return err;
+  }
   AdrenoMemInfo* adreno_mem_info = AdrenoMemInfo::GetInstance();
   graphics_metadata->size = adreno_mem_info->AdrenoGetMetadataBlobSize();
   uint64_t adreno_usage = info.usage;
