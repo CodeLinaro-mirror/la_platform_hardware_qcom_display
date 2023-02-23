3cd581a49ebe3e1c6f45a65b588a1a5a7de56310
Devanshi Bansal <quic_devanshi@quicinc.com>
2023-02-23 13:56:51 +0530

    composer: add configs with mode switch not allowed in different group
    Change-Id: I95c0197ab3a59e884b9ce49068a145f77a2ffd2e

--- composer/hwc_display.cpp
+++ composer/hwc_display.cpp
@@ -53,6 +53,12 @@
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+* Changes from Qualcomm Innovation Center are provided under the following license:
+* Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+  SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
 #include <cutils/properties.h>
 #include <errno.h>
 #include <math.h>
@@ -1190,7 +1196,7 @@ HWC2::Error HWCDisplay::GetDisplayAttribute(hwc2_config_t config, HwcAttribute a
       *out_value = INT32(variable_config.y_dpi * 1000.0f);
       break;
     case HwcAttribute::CONFIG_GROUP:
-      *out_value = GetDisplayConfigGroup(variable_config);
+      *out_value = GetDisplayConfigGroup(config);
       break;
     default:
       DLOGW("Spurious attribute type = %s", composer_V2_4::toString(attribute).c_str());
@@ -2802,11 +2808,10 @@ void HWCDisplay::UpdateActiveConfig() {
   pending_config_ = false;
 }
 
-int32_t HWCDisplay::GetDisplayConfigGroup(DisplayConfigGroupInfo variable_config) {
-  for (auto &config : variable_config_map_) {
-    DisplayConfigGroupInfo const &group_info = config.second;
-    if (group_info == variable_config) {
-      return INT32(config.first);
+int32_t HWCDisplay::GetDisplayConfigGroup(hwc2_config_t config) {
+  for (auto &group_config : variable_config_map_) {
+    if (IsSameGroup(config, group_config.first)) {
+      return INT32(group_config.first);
     }
   }
 
@@ -3022,10 +3027,16 @@ bool HWCDisplay::IsSameGroup(hwc2_config_t config_id1, hwc2_config_t config_id2)
     return false;
   }
 
+  if (variable_config1 == variable_config2) {
+    return true;
+  }
+
   const DisplayConfigGroupInfo &config_group1 = variable_config1->second;
   const DisplayConfigGroupInfo &config_group2 = variable_config2->second;
 
-  return (config_group1 == config_group2);
+  return ((config_group1 == config_group2) &&
+          (config_group1.allowed_mode_switch & (1 << (INT32(config_id2)))) &&
+          (config_group2.allowed_mode_switch & (1 << (INT32(config_id1)))));
 }
 
 bool HWCDisplay::AllowSeamless(hwc2_config_t config) {
