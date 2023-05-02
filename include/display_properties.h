549ed7296778923fbced443135914aa31dc51d2a
Devanshi Bansal <quic_devanshi@quicinc.com>
2023-05-02 14:30:45 +0530

    include: Add property to set xr variant from display-test
    Change-Id: I7e7b6965f2c3918bdb51061bb5f41c7af893ad2c

--- include/display_properties.h
+++ include/display_properties.h
@@ -233,6 +233,7 @@
 #define PRIORITIZE_CLIENT_CWB                DISPLAY_PROP("prioritize_client_cwb")
 #define TRANSIENT_FPS_CYCLE_COUNT            DISPLAY_PROP("transient_fps_cycle_count")
 #define CORE_ID_MASK                         DISPLAY_PROP("core_id_mask")
+#define XR_VARIANT                           DISPLAY_PROP("xr_variant")
 
 // Add all other.properties above
 // End of property
