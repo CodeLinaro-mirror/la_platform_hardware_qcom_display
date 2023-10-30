6f45b7ca5020d7ab52862192b2cf497dbec3ad8b
Rheygine Medel <quic_rmedel@quicinc.com>
2023-10-30 10:56:35 -0700

    init: Add default properties for sun
    Change-Id: I3e5f438472b4fe6effcbabb56c5ac39facba73b8
    CRs-Fixed: 3651722

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -37,6 +37,27 @@ else
 fi
 
 case "$target" in
+    "sun")
+    #SOC ID for Sun is 618
+    case "$soc_hwid" in
+      618)
+        setprop vendor.display.enable_fb_scaling 0
+        setprop vendor.gralloc.use_dma_buf_heaps 1
+        setprop vendor.display.target.version 4
+        setprop vendor.display.enable_posted_start_dyn 2
+        setprop vendor.display.enable_allow_idle_fallback 1
+        setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_rotator_ui 1
+        setprop vendor.display.enable_spec_fence 1
+        setprop vendor.display.thermal.version 1
+        setprop vendor.display.enable_rc_support 1
+        setprop vendor.display.enable_latch_media_content 1
+        setprop vendor.display.enable_inline_writeback 1
+        setprop vendor.display.timed_render_enable 1
+        setprop vendor.gralloc.hw_supports_ubwcp 0
+        ;;
+    esac
+    ;;
     "pineapple")
     #SOC ID for Pineapple is 557
     case "$soc_hwid" in
