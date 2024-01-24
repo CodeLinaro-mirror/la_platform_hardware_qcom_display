caa1cb36b5d294ae5291b93787a5aee9e40e1bdf
Avdhesh Singh Chauhan <quic_avdheshs@quicinc.com>
2024-01-24 13:12:03 +0000

    sdm: Add soc id for Anorak61.
    Change-Id: I400f17f5ba9565d325d842c07c9f4148c9203e11
    Signed-off-by: Avdhesh Singh Chauhan <quic_avdheshs@quicinc.com>

--- sdm/include/utils/soc_info.h
+++ sdm/include/utils/soc_info.h
@@ -1,12 +1,13 @@
 /*
-* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
 #ifndef __SOC_INFO_H__
 #define __SOC_INFO_H__
 
-#define kAnorakSocId 549
+#define kAnorakSocId1 549
+#define kAnorakSocId2 649
 #define kAnorakSocName "anorak"
 
 #endif  // __SOC_INFO_H__
