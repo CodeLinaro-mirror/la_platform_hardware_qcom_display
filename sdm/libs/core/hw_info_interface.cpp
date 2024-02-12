a19b01e4d6e316a2851239a0c6b186bc7431eba1
Tushar Shivajirao Ghadge <quic_tghadge@quicinc.com>
2024-02-12 15:23:46 +0530

    sdm : add DRMLibLoader instance for each DPU
    adding DRMLibLoader instance for each DPU because
    DRMLibLoader::GetInstance() was returning a singleton instance,
    when DRMLibLoader::Destroy was called by one DPU,
    all the DPUs were getting affected with crash as only one instance
    of DRMLibLoader was available.
    
    Change-Id: I75ac92bf70c6e7592920370db175564502851bc5
    Signed-off-by: Tushar Shivajirao Ghadge <quic_tghadge@quicinc.com>
    (cherry picked from commit 2d2125f4dc3cec39083c6a6010e4d0e1c9da231a)

--- sdm/libs/core/hw_info_interface.cpp
+++ sdm/libs/core/hw_info_interface.cpp
@@ -30,7 +30,7 @@
 /*
  *  Changes from Qualcomm Innovation Center are provided under the following license:
  *
- *  Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted (subject to the limitations in the
@@ -79,6 +79,7 @@ DisplayError HWInfoInterface::Create(std::vector<HWInfoInterface*> *intfs) {
       error = hw_info->Init();
       if (error != kErrorNone) {
         delete hw_info;
+        hw_info = NULL;
         return intfs->size() ? kErrorNone : error;
       }
       intfs->push_back(hw_info);
