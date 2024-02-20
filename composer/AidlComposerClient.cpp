27ef8f3c29f12ec40e7d3d90ce51fb7a4591188f
John Tareco <quic_jtareco@quicinc.com>
2024-02-20 10:03:36 -0800

    Change QServiceBackend to static instance
    Change-Id: Iea60cb4b8322a19d180944a0e42b97aaa8886880

--- composer/AidlComposerClient.cpp
+++ composer/AidlComposerClient.cpp
@@ -97,7 +97,8 @@ bool AidlComposerClient::init(SDMDisplayCapsIntf *caps,
 
   lifecycle_->Init(this, &buffer_allocator_, &socket_handler_);
 
-  qservice_ = new QServiceBackend();
+  qservice_ = QServiceBackend::GetInstance();
+  qservice_->Init();
 
   mCommandEngine = std::make_unique<CommandEngine>(*this);
   if (mCommandEngine == nullptr) {
@@ -131,7 +132,6 @@ AidlComposerClient::~AidlComposerClient() {
   ALOGW("%s: Destroying composer client", __FUNCTION__);
 
   lifecycle_->EnableCallback(false);
-  delete qservice_;
 
   // no need to grab the mutex as any in-flight hwbinder call would have
   // kept the client alive
