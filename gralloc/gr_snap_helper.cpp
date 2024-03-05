5c99e9461345416b343c927a7ee026ac472ffbac
Dheepthi S <quic_dheepthi@quicinc.com>
2024-03-05 20:24:19 +0530

    gralloc: support PQ transfer in dataspace
    Google start to query color dataspace from gralloc if not
    produced by APP in screen capture. In HDR playback case,
    gralloc return unsupport for PQ transfer.
    Change adds that return correct value for PQ transfer.
    
    Change-Id: I15b8d720e5685ad53855f27f356cafddeb12f927

--- gralloc/gr_snap_helper.cpp
+++ gralloc/gr_snap_helper.cpp
@@ -2609,6 +2609,9 @@ int GrallocSnapHelper::ConvertSnapDataspaceToGrallocDataspace(SnapDataspace &sna
     case QtiTransfer_HLG:
       transfer = GrallocDataspace::TRANSFER_HLG;
       break;
+    case QtiTransfer_SMPTE_ST2084:
+      transfer = GrallocDataspace::TRANSFER_ST2084;
+      break;
     default:
       ALOGW("%s: Failed to convert transfer %d", __FUNCTION__, snap_dataspace.transfer);
       return SnapError::BAD_VALUE;
@@ -2770,6 +2773,9 @@ int GrallocSnapHelper::ConvertGrallocDataspaceToSnapDataspace(GrallocDataspace g
       break;
     case (uint32_t)GrallocDataspace::TRANSFER_HLG:
       dataspace.transfer = QtiTransfer_HLG;
+      break;
+    case (uint32_t)GrallocDataspace::TRANSFER_ST2084:
+      dataspace.transfer = QtiTransfer_SMPTE_ST2084;
       break;
     default:
       ALOGW("%s: Failed to convert transfer %d", __FUNCTION__, transfer);
@@ -6574,6 +6580,9 @@ int GrallocSnapHelperLegacy::ConvertSnapDataspaceToGrallocDataspace(
     case QtiTransfer_HLG:
       transfer = GrallocDataspace::TRANSFER_HLG;
       break;
+    case QtiTransfer_SMPTE_ST2084:
+      transfer = GrallocDataspace::TRANSFER_ST2084;
+      break;
     default:
       ALOGW("%s: Failed to convert transfer %d", __FUNCTION__, snap_dataspace.transfer);
       return SnapError::BAD_VALUE;
@@ -6735,6 +6744,9 @@ int GrallocSnapHelperLegacy::ConvertGrallocDataspaceToSnapDataspace(GrallocDatas
       break;
     case (uint32_t)GrallocDataspace::TRANSFER_HLG:
       dataspace.transfer = QtiTransfer_HLG;
+      break;
+    case (uint32_t)GrallocDataspace::TRANSFER_ST2084:
+      dataspace.transfer = QtiTransfer_SMPTE_ST2084;
       break;
     default:
       ALOGW("%s: Failed to convert transfer %d", __FUNCTION__, transfer);
