8a0f12b97d67c6a78659aa968b0edb8817f3a1d6
John Tareco <quic_jtareco@quicinc.com>
2024-03-14 17:19:53 -0700

    hqd: add concrete impl for new debug callback intf
    Snapalloc and sdmclient will have separate instances for their
    debug classes
    
    Change-Id: Ic2f5313dbc5fdb19c22dfcd24d447095d1f0b073

---
+++ composer/hwc_debugger.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef __HWC_DEBUGGER_H__
+#define __HWC_DEBUGGER_H__
+
+#include <unordered_map>
+#include <log/log.h>
+#include <utils/Trace.h>
+#include <core/sdm_types.h>
+
+#include "debug_callback_intf.h"
+
+namespace sdm {
+
+class HWCDebugger : public DebugCallbackIntf {
+ public:
+  void Log(DebugLogType type, const char *log_tag, const char *fmt, std::va_list &args);
+  int GetProperty(const char *property_name, int *value);
+  int GetProperty(const char *property_name, char *value);
+  void BeginTrace(const char *class_name, const char *function_name, const char *custom_string);
+  void EndTrace();
+  void ATrace(const char *custom_string, const int bit);
+
+ private:
+  std::unordered_map<DebugLogType, android_LogPriority> log_type_map_ = {
+      {ERROR, ANDROID_LOG_ERROR},
+      {WARNING, ANDROID_LOG_WARN},
+      {INFO, ANDROID_LOG_INFO},
+      {DEBUG, ANDROID_LOG_DEBUG},
+      {VERBOSE, ANDROID_LOG_VERBOSE}};
+};
+
+}  // namespace sdm
+
+#endif  // __HWC_DEBUGGER_H__
