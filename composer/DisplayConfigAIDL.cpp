73dc67e0a9547ae7cc22ef3ed4d0dfbeebe00dc4
Jagdish Prajapati <quic_jprajapa@quicinc.com>
2024-07-11 18:00:57 +0530

    composer: Make thread safe CWB client request and notification APIs
    - Make thread safe update and retrieval of CWB unordered callback map.
    - Add display id to callback map to retrieve back while client
      notification to append in notification log.
    
    Change-Id: I655915aea0ecef5c93174338fee3e2d04e09834e

--- composer/DisplayConfigAIDL.cpp
+++ composer/DisplayConfigAIDL.cpp
@@ -705,21 +705,23 @@ ScopedAStatus DisplayConfigAIDL::setCWBOutputBuffer(
   roi.right = FLOAT(rect.right);
   roi.bottom = FLOAT(rect.bottom);
 
-  ALOGI("CWB config passed by cwb_client : tappoint %d  CWB_ROI : (%f %f %f %f)",
-        cwb_config.tap_point, roi.left, roi.top, roi.right, roi.bottom);
+  ALOGI("CWB config passed by cwb_client : tappoint %d  CWB_ROI : (%f %f %f %f) for display-%d",
+        cwb_config.tap_point, roi.left, roi.top, roi.right, roi.bottom, display_type);
 
   auto ret_status = EX_NONE;
 
   void *hdl = sdm::ConvertToSnapHandle(buffer);
 
-  if (!handle_importer_.importBuffer(static_cast<const SnapHandle *>(hdl))) {
-    ALOGE("%s: Snapmapper retain failed.", __FUNCTION__);
+  if (!hdl || !handle_importer_.importBuffer(static_cast<const SnapHandle *>(hdl))) {
+    ALOGE("%s: Either retrieving snaphandle or importing buffer failed.", __FUNCTION__);
     ret_status = EX_ILLEGAL_ARGUMENT;
   }
 
-  if (ret_status == EX_NONE && cwb_callbacks_.find(hdl) != cwb_callbacks_.end()) {
-    ALOGE("%s: buffer already being handled", __FUNCTION__);
-    ret_status = EX_ILLEGAL_ARGUMENT;
+  bool hdl_exists = false;
+  if (ret_status == EX_NONE) {
+    std::lock_guard<decltype(cwb_callbacks_lock_)> lock_guard(cwb_callbacks_lock_);
+    hdl_exists = cwb_callbacks_.find(hdl) != cwb_callbacks_.end();
+    ret_status = (hdl_exists) ? EX_ILLEGAL_ARGUMENT : EX_NONE;
   }
 
   if (ret_status == EX_NONE) {
@@ -727,8 +729,11 @@ ScopedAStatus DisplayConfigAIDL::setCWBOutputBuffer(
     if (ret != sdm::kErrorNone) {
       ret_status = EX_TRANSACTION_FAILED;
     } else {
-      cwb_callbacks_.insert({hdl, callback});
+      std::lock_guard<decltype(cwb_callbacks_lock_)> lock_guard(cwb_callbacks_lock_);
+      cwb_callbacks_.insert({hdl, {display_type, callback}});
     }
+  } else if (hdl_exists) {
+    ALOGE("%s: buffer(0x%x) already being handled by display-%d", __FUNCTION__, hdl, display_type);
   }
 
   if (ret_status != EX_NONE) {
@@ -741,22 +746,32 @@ ScopedAStatus DisplayConfigAIDL::setCWBOutputBuffer(
 }
 
 void DisplayConfigAIDL::NotifyCWBStatus(int32_t status, void *hdl) {
-  if (cwb_callbacks_.find(hdl) == cwb_callbacks_.end()) {
-    ALOGE("%s: buffer not found", __FUNCTION__);
+  std::shared_ptr<IDisplayConfigCallback> callback = nullptr;
+  int32_t display_type = 0;
+
+  if (!hdl) {
+    ALOGE("%s: Null buffer handle is detected to notify!", __FUNCTION__);
     return;
+  } else {
+    std::lock_guard<decltype(cwb_callbacks_lock_)> lock_guard(cwb_callbacks_lock_);
+    auto hdl_exists = cwb_callbacks_.find(hdl) != cwb_callbacks_.end();
+    if (hdl_exists) {
+      std::tie(display_type, callback) = cwb_callbacks_[hdl];
+      cwb_callbacks_.erase(hdl);
+    }
   }
 
-  std::shared_ptr<IDisplayConfigCallback> callback = cwb_callbacks_[hdl];
-
-  NativeHandle buffer =
-      sdm::AIDLNativeHandleFromSnapHandle(reinterpret_cast<SnapHandle *>(hdl), false);
-  if (callback) {
-    ALOGI("Notify the client about buffer status %d.", status);
+  if (!callback) {
+    ALOGE("%s: buffer handle(0x%x) not found", __FUNCTION__, hdl);
+  } else {
+    NativeHandle buffer =
+        sdm::AIDLNativeHandleFromSnapHandle(reinterpret_cast<SnapHandle *>(hdl), false);
+    ALOGI("%s: Notify the client about buffer (0x%x) status %d for display-%d.", __FUNCTION__, hdl,
+          status, display_type);
 
     callback->notifyCWBBufferDone(status, buffer);
   }
 
-  cwb_callbacks_.erase(hdl);
   handle_importer_.freeBuffer(static_cast<const SnapHandle *>(hdl));
 }
 
