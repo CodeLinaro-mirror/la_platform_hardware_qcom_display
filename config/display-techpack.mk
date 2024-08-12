7f75dae41c60d61d1c96cda0b4e5289b4e808702
lnxdisplay <lnxdisplay@localhost>
2024-08-13 01:23:44 +0530

    Revert "Task:tp tools sunset."
    This reverts commit cdc02177f2c5abf6689403218ff3c01990be5029.
    
    Reason for revert: <Unintentional merge breaks trunk workflow>
    
    Change-Id: I417c47198682cafe6b8f11d005294acc38771161

--- config/display-techpack.mk
+++ config/display-techpack.mk
@@ -6,17 +6,13 @@ ifneq (,$(wildcard $(QCPATH)/display))
 include $(QCPATH)/display/config/display-vendor-modules.mk
 endif
 
-.PHONY: display_tp display_tp_hal display_tp_dlkm display_tp_tests display_tp_feat
+.PHONY: display_tp display_tp_hal display_tp_dlkm
 
-display_tp: display_tp_hal display_tp_dlkm display_tp_tests display_tp_feat
+display_tp: display_tp_hal display_tp_dlkm
 
 display_tp_hal: $(DISPLAY_MODULES_HARDWARE) $(DISPLAY_MODULES_VENDOR)
 
 display_tp_dlkm: $(DISPLAY_MODULES_DRIVER) $(DISPLAY_MM_DRIVER)
-
-display_tp_tests: $(DISPLAY_MODULES_TEST)
-
-display_tp_feat: $(DISPLAY_MODULES_FEAT)
 
 $(warning "Display Techpack configuration TARGET_USES_QMAA  = $(TARGET_USES_QMAA)")
 $(warning "Display Techpack configuration TARGET_USES_QMAA_OVERRIDE_DISPLAY  = $(TARGET_USES_QMAA_OVERRIDE_DISPLAY)")
