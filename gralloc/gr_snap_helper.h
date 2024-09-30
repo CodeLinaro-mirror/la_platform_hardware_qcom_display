128b3155f9ed6fca6a61c75e38d8ba6c1ab206a0
Yashovardhan Bhatt <quic_ybhatt@quicinc.com>
2024-09-30 19:57:59 +0530

    gralloc: Add RGBA5551 and RGBA4444 mapping to gr_snap_helper
    Add RGBA4444 and RGBA5551 to gr_snap_helper
    format conversion maps.
    
    Change-Id: Ic4c7a20746b8eeec043b5b3d1c79a6ed82f30ad1
    CRs-Fixed: 3685818
    (cherry picked from commit ee35a427302d69a514053981785b7d8664b3b9b5)

--- gralloc/gr_snap_helper.h
+++ gralloc/gr_snap_helper.h
@@ -419,7 +419,12 @@ class GrallocSnapHelper : public GrallocSnapHelperIntf {
           {{.format = SnapPixelFormat::BGRX_1010102, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
            HAL_PIXEL_FORMAT_BGRX_1010102},
           {{.format = SnapPixelFormat::BGR_888, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
-           HAL_PIXEL_FORMAT_BGR_888}};
+           HAL_PIXEL_FORMAT_BGR_888},
+	   {{.format = SnapPixelFormat::RGBA_5551, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
+           HAL_PIXEL_FORMAT_RGBA_5551},
+          {{.format = SnapPixelFormat::RGBA_4444, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
+           HAL_PIXEL_FORMAT_RGBA_4444},
+      };
 
   std::unordered_map<SnapFormatDescriptor, SnapPixelFormat, SnapFormatDescriptorHash>
       snap_to_flat_format_ = {
@@ -539,7 +544,12 @@ class GrallocSnapHelper : public GrallocSnapHelperIntf {
           {{.format = SnapPixelFormat::BGRX_1010102, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
            SnapPixelFormat::BGRX_1010102},
           {{.format = SnapPixelFormat::BGR_888, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
-           SnapPixelFormat::BGR_888}};
+           SnapPixelFormat::BGR_888},
+	   {{.format = SnapPixelFormat::RGBA_5551, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
+           SnapPixelFormat::RGBA_5551},
+          {{.format = SnapPixelFormat::RGBA_4444, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
+           SnapPixelFormat::RGBA_4444},
+     };
 
   std::unordered_map<SnapFormatDescriptor, SnapPixelFormat, SnapFormatDescriptorHash>
       snap_to_flat_ubwc_format_ = {
@@ -1355,6 +1365,10 @@ class GrallocSnapHelperLegacy : public GrallocSnapHelperIntf {
            HAL_PIXEL_FORMAT_COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR},
           {{.format = SnapPixelFormat::Y16, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
            static_cast<int>(PixelFormat::Y16)},
+          {{.format = SnapPixelFormat::RGBA_5551, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
+           HAL_PIXEL_FORMAT_RGBA_5551},
+          {{.format = SnapPixelFormat::RGBA_4444, .modifier = PIXEL_FORMAT_MODIFIER_NONE},
+           HAL_PIXEL_FORMAT_RGBA_4444},
       };
 
   std::unordered_map<uint64_t, SnapFormatDescriptor> gralloc_to_snap_format_;
