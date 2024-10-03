9bd12c15432a8868f9348666b99bfbf72a5bff50
Yashovardhan Bhatt <quic_ybhatt@quicinc.com>
2024-10-03 11:34:45 +0530

    Revert " gralloc: Fix P010 alignment and plane layout."
    This reverts commit 15ab87ce95f812b8d51f0c480c4c5cfd352bd75a.
    
    Change-Id: I929a76ab288a34757fc8bed1e049d210739a4710
    Signed-off-by: Yashovardhan Bhatt <quic_ybhatt@quicinc.com>

--- gralloc/gr_utils.cpp
+++ gralloc/gr_utils.cpp
@@ -363,6 +363,8 @@ unsigned int GetSize(const BufferInfo &info, unsigned int alignedw, unsigned int
         size = ALIGN((alignedw * alignedh) + (alignedw * alignedh) / 2 + 1, SIZE_4K);
         break;
       case HAL_PIXEL_FORMAT_YCbCr_420_P010:
+        size = ALIGN((alignedw * alignedh * 2) + (alignedw * alignedh) + 1, SIZE_4K);
+        break;
       case HAL_PIXEL_FORMAT_YCbCr_420_P010_VENUS:
 #ifdef __MIN_ANDROID_VER_T__
         size = MMM_COLOR_FMT_BUFFER_SIZE(MMM_COLOR_FMT_P010, width, height);
@@ -622,6 +624,10 @@ void GetYuvSPPlaneInfo(const BufferInfo &info, int format, uint32_t width, uint3
       break;
     case HAL_PIXEL_FORMAT_Y8:
       c_size = 0;
+      break;
+    case HAL_PIXEL_FORMAT_YCbCr_420_P010:
+      c_size = (width * height) + 1;
+      c_height = height;
       break;
     default:
       break;
@@ -1199,9 +1205,9 @@ int GetAlignedWidthAndHeight(const BufferInfo &info, unsigned int *alignedw,
     case HAL_PIXEL_FORMAT_YCrCb_422_SP:
     case HAL_PIXEL_FORMAT_YCbCr_422_I:
     case HAL_PIXEL_FORMAT_YCrCb_422_I:
+    case HAL_PIXEL_FORMAT_YCbCr_420_P010:
       aligned_w = ALIGN(width, 16);
       break;
-    case HAL_PIXEL_FORMAT_YCbCr_420_P010:
     case HAL_PIXEL_FORMAT_YCbCr_420_P010_VENUS:
 #ifdef __MIN_ANDROID_VER_T__
       aligned_w = INT(MMM_COLOR_FMT_Y_STRIDE(MMM_COLOR_FMT_P010, width) / 2);
@@ -1623,6 +1629,18 @@ int GetYUVPlaneInfo(const BufferInfo &info, int32_t format, int32_t width, int32
       }
       break;
 
+    case HAL_PIXEL_FORMAT_YCbCr_420_P010:
+      *plane_count = 2;
+      GetYuvSPPlaneInfo(info, format, width, height, 2, plane_info);
+      GetYuvSubSamplingFactor(format, &h_subsampling, &v_subsampling);
+      plane_info[0].h_subsampling = 0;
+      plane_info[0].v_subsampling = 0;
+      plane_info[0].step = 2;
+      plane_info[1].h_subsampling = h_subsampling;
+      plane_info[1].v_subsampling = v_subsampling;
+      plane_info[1].step = 4;
+      break;
+
     case HAL_PIXEL_FORMAT_YCbCr_420_TP10_UBWC:
       *plane_count = 4;
 #ifdef __MIN_ANDROID_VER_T__
@@ -1661,7 +1679,6 @@ int GetYUVPlaneInfo(const BufferInfo &info, int32_t format, int32_t width, int32
       plane_info[2].step = plane_info[3].step = 0;
       break;
 
-    case HAL_PIXEL_FORMAT_YCbCr_420_P010:
     case HAL_PIXEL_FORMAT_YCbCr_420_P010_VENUS:
       *plane_count = 2;
 #ifdef __MIN_ANDROID_VER_T__
