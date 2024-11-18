922ed013e81865515d98d44185945dbd855bec55
Avdhesh Singh Chauhan <quic_avdheshs@quicinc.com>
2024-11-18 14:29:20 +0530

    config: Adjust Phase offset for niobe.
    Change-Id: Ibf4b3c0d6d6f4a58704943471436d55753d34b4e

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -118,7 +118,7 @@ case "$target" in
         setprop vendor.display.enable_rotator_ui 1
         setprop vendor.display.thermal.version 1
         setprop vendor.display.enable_rc_support 0
-        setprop vendor.display.target.version 4
+        setprop vendor.display.target.version 5
         setprop vendor.display.disable_mitigated_fps 1
         setprop vendor.display.disable_cwb_idle_fallback 1
         setprop vendor.display.enable_rounded_corner 0  #disable HW RC
