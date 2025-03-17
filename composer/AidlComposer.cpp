28f379a99a0e1d87a6965d3f8af60d34d6e4d9d7
Mohit Sharma <quic_smohit@quicinc.com>
2025-03-17 19:14:44 +0530

    Revert "hqd: Cancel init on error"
    This reverts commit 295604f63d065f9482ce86330f4cb27d549babec.
    
    Change-Id: I6abc4a6e8b8dfc4f66dd9902626328a055023078

--- composer/AidlComposer.cpp
+++ composer/AidlComposer.cpp
@@ -16,7 +16,7 @@
 
 /*
  * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -52,18 +52,9 @@ AidlComposer::AidlComposer(const shared_ptr<QtiComposer3Client> &extensions)
   qservice_->Init();
 
   // initialize SDMClient
-  auto error = lifecycle_->Init(&buffer_allocator_, &socket_handler_, &hwc_debugger_);
-  if (error) {
-    ALOGE("Failed to initialize SDMClient!");
-    caps_ = nullptr;
-    settings_ = nullptr;
-    lifecycle_ = nullptr;
-    drawcycle_ = nullptr;
-    layers_ = nullptr;
-    sideband_ = nullptr;
-  } else {
-    ALOGI("Created AidlComposer");
-  }
+  lifecycle_->Init(&buffer_allocator_, &socket_handler_, &hwc_debugger_);
+
+  ALOGI("Created AidlComposer");
 }
 
 AidlComposer::~AidlComposer() {}
