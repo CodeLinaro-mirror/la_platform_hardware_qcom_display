d66941ea2c8d103ff1c463bc3b746c6383b72fcb
Tuhin Paul <quic_tuhipaul@quicinc.com>
2025-08-13 10:43:46 +0530

    config: DCP-FW prebuilt config changes
    Logically Select the Binary and ELF names
    
    Change-Id: Ibf3217f33007345f48b0b109903bf977a9d24884

--- config/display-modules.mk
+++ config/display-modules.mk
@@ -20,7 +20,10 @@ ifneq ($(TARGET_IS_HEADLESS),true)
 		DISPLAY_MODULES_HARDWARE += libcomposertestservice
 	endif
 
-    DISPLAY_FW_PREBUILTS = dcp.bin dcp_elf
+    ifneq ($(filter canoe seraph vienna, $(TARGET_BOARD_PLATFORM)),)
+        DISPLAY_FW_BUILD_TYPE := $(if $(wildcard $(QCPATH)/display-noship),test,pack)
+        DISPLAY_FW_PREBUILTS = dcp_bin_$(TARGET_BOARD_PLATFORM)_$(DISPLAY_FW_BUILD_TYPE) dcp_elf_$(TARGET_BOARD_PLATFORM)_$(DISPLAY_FW_BUILD_TYPE)
+    endif
 
     DISPLAY_MODULES_HARDWARE += libsdmcore \
                             libsdmdal \
