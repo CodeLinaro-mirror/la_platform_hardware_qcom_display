b6aa6f492c4b1edcab840b69c75c72d1b1acff62
BOREDDY MAHIDHAR <quic_bmahidha@quicinc.com>
2025-08-20 20:56:10 +0530

    composer: Add support for SetLayerLuts
    Change-Id: Id03eb428a1fc5aed3952f5eed5ed1515c339b367

--- composer/AidlComposerClient.h
+++ composer/AidlComposerClient.h
@@ -431,6 +431,9 @@ class AidlComposerClient : public BnComposerClient,
     void executeSetLayerBlockingRegion(int64_t display, int64_t layer,
                                        const std::vector<std::optional<Rect>> &blockingRegion);
     void executeSetFrameIntervalNsInternal(int64_t display, int32_t frameIntervalNs);
+#ifdef COMPOSER3_V4
+    void executeSetLayerLuts(int64_t display, int64_t layer, const Luts &luts);
+#endif
 
     // Commands from extensions (QtiComposer3Client)
     void executeSetClientTarget_3_1(int64_t display, const ClientTarget &command);
