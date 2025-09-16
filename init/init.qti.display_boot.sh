1de70d3767c2117b10d166120398880d4f7376d9
Shreyas Narayan <shrnar@qti.qualcomm.com>
2025-09-16 02:28:37 -0700

    Revert "init: add socid for kera"
    This reverts commit 23fdeadb1c515fcded7f3d7b40b167c0683fd3e4.
    
    Reason for revert: <Change will be brought in via mainline re-target>
    
    Change-Id: Ifcd200517b71f404b56b2f752b7111d62a253b22

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -66,14 +66,12 @@ case "$target" in
         setprop vendor.display.enable_hal_self_refresh 1
         setprop vendor.display.cpu_cluster_boost_mask 3
         ;;
-      655|681|659|694|686|720|721)
+      655|681|659|694|686)
         #SOC ID for tuna is 655
         #SOC ID for tuna7 is 681
         #SOC ID for tuna APQ is 694
         #SOC ID for kera is 659
         #SOC ID for kera is 686
-        #SOC ID for kera is 720
-        #SOC ID for kera is 721
         setprop vendor.display.enable_fb_scaling 0
         setprop vendor.gralloc.use_dma_buf_heaps 1
         setprop vendor.display.target.version 6
