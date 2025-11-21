480f68528cc4aa367a93f0e0adaea1fef4763699
Chintankumar Savaliya <csavaliy@qti.qualcomm.com>
2025-11-21 16:44:44 +0530

    init: enable null display for hamoa qcb
    Change-Id: Ib115ec3f929df783463584ea11cc42f38526f783

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -32,6 +32,7 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
 target=`getprop ro.board.platform`
+platform_subtype_id=0
 if [ -f /sys/devices/soc1/soc_id ]; then
     soc_hwid=`cat /sys/devices/soc1/soc_id`
 elif [ -f /sys/devices/system/soc/soc1/id ]; then
@@ -40,6 +41,12 @@ elif [ -f /sys/devices/soc0/soc_id ]; then
     soc_hwid=`cat /sys/devices/soc0/soc_id`
 else
     soc_hwid=`cat /sys/devices/system/soc/soc0/id`
+fi
+
+if [ -f /sys/devices/soc1/platform_subtype_id ]; then
+    platform_subtype_id=`cat /sys/devices/soc1/platform_subtype_id`
+elif [ -f /sys/devices/soc0/platform_subtype_id ]; then
+    platform_subtype_id=`cat /sys/devices/soc0/platform_subtype_id`
 fi
 
 case "$target" in
@@ -126,7 +133,11 @@ case "$target" in
         else
            setprop vendor.display.disable_cwb_idle_fallback 1
         fi
+        # Enable null display for Hamoa QCB and set Hamoa-specific properties
         if [ "$soc_hwid" -eq 555 ]; then
+          if [ "$platform_subtype_id" -eq 43 ]; then
+            setprop vendor.display.enable_null_display 1
+          fi
           setprop vendor.display.disable_dpps_features 1
           setprop vendor.display.disable_hdr_lut_gen 1
         fi
