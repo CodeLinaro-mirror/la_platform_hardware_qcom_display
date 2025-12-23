0022da5c550af89d98d21c4375bb4af5c2e462e9
Pritam Samanta <pritsama@qti.qualcomm.com>
2025-12-23 10:48:20 +0530

    display : disable layer stitch for memory optimization
    Change-Id: Ic4964ff4e6e0055b17f69e129b5e5660400ed8df

--- config/display-product.mk
+++ config/display-product.mk
@@ -159,6 +159,7 @@ endif
 ifeq ($(TARGET_HAS_QTI_OPTIMIZATIONS), true)
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_cache_manager=1 \
+    vendor.display.disable_layer_stitch=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=2
 endif
 
