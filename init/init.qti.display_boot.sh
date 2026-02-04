ac244bd5ec8b3691f8746b9124c29788527a441b
John Tareco <jtareco@qti.qualcomm.com>
2026-02-04 06:32:30 -0800

    art: Enable inline writeback (iwe) by default
    Change-Id: I551e1db82278607454f3a2fe5a9628ad3fe69102
    CRs-Fixed: 4424713

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -68,6 +68,7 @@ case "$target" in
         setprop vendor.display.force_gpu_composition 0
         setprop vendor.display.enable_spec_fence 1
         setprop vendor.display.enable_perf_hint_large_comp_cycle 1
+        setprop vendor.display.enable_inline_writeback 1
         ;;
     esac
     ;;
