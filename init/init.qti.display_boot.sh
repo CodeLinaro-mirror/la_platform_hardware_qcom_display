d64d4949a154033f72b6491a3a89ca8f11166b53
Jatin Srivastava <jsrivast@qti.qualcomm.com>
2026-02-24 17:09:55 +0530

    init: Remove disable cwb idle fallback vendor property
    This change removes vendor prop disable_cwb_idle_fallback
    in art target.
    
    Change-Id: I35481e14430b196683afbaf03ef2c9c1d4bc7976

--- init/init.qti.display_boot.sh
+++ init/init.qti.display_boot.sh
@@ -64,7 +64,6 @@ case "$target" in
         setprop vendor.display.thermal.version 1
         setprop vendor.gralloc.enable_snapalloc 1
         setprop vendor.display.refresh_rate_changeable 1
-        setprop vendor.display.disable_cwb_idle_fallback 0
         setprop vendor.display.disable_pu_ds 1
         setprop vendor.display.force_gpu_composition 0
         setprop vendor.display.enable_spec_fence 1
