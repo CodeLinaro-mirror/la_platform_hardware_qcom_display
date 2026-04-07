00c36dfadaea69e1ea9e7fed13d84ac761440cb8
Abhishek Singh Tomar <asinghto@qti.qualcomm.com>
2026-04-06 22:20:29 -0700

    composer: serialize destroyLayer against executeCommands to fix race
    Fix a race condition where destroyLayer (Binder B) could free an
    SDMLayer while executeCommands (Binder A) was still operating on the
    same layer stack.
    
    Change-Id: I58c0760a1dcea3bd930f5c36a0012dedb7904070

--- composer/AidlComposerClient.cpp
+++ composer/AidlComposerClient.cpp
@@ -236,6 +236,8 @@ ScopedAStatus AidlComposerClient::destroyLayer(int64_t in_display, int64_t in_la
 
   ALOGV("%s: destroyLayer called out of LLCBC group for layer %" PRId64 " on display-%" PRId64 ".", __FUNCTION__,
         in_layer, in_display);
+
+  std::lock_guard<std::mutex> cmd_lock(m_command_mutex_);
   drawcycle_->WaitForDrawCycleToComplete(in_display);
   auto error = layer_builder_->DestroyLayer(in_display, in_layer);
   drawcycle_->LayerStackUpdated(in_display);
