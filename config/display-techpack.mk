f1121ea910d862b8451c5f88a613582523f6e7e1
Ramesh Velpula <ramevelp@qti.qualcomm.com>
2026-04-22 19:49:26 +0530

    Add TARGET_BOARD_SUFFIX support
    Change-Id: Iee10ad790da503946d83f82a493cbbdd94b3521d

--- config/display-techpack.mk
+++ config/display-techpack.mk
@@ -6,6 +6,7 @@ include vendor/qcom/opensource/display-drivers/display_driver_product.mk
 endif
 
 -include $(QCPATH)/techpack/artifacts/display/$(TARGET_BOARD_PLATFORM)/prebuilt.mk
+-include $(QCPATH)/techpack/artifacts/display/$(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)/prebuilt.mk
 
 ifneq (,$(wildcard $(QCPATH)/display))
 include $(QCPATH)/display/config/display-vendor-modules.mk
